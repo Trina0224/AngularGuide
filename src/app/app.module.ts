6d35b72e8fd450ae6516ea91a1f6fbb6aa2674e4
Hsin Yun Shih <Trina@Hsins-MacBook.local>
2020-07-31 11:04:45 -0700

    template for future

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,8 +11,8 @@ import { FormsModule } from '@angular/forms';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
+    //FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
